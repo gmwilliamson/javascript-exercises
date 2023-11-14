3e48170da345c0b6d8e1d3d66bd6ccea37764a5d
Greg Williamson <greg.williamson@qualio.com>
2023-11-13 19:33:25 -0500

    Complete 10_fibonacci exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+    
+    let x = 1;
+    let y = 0;
 
-};
+    for (let i = 2; i <= num; i++) {
+        let current = x + y;
+        y = x;
+        x = current;
+    }
 
+    return x;
+
+}
+   
 // Do not edit below this line
 module.exports = fibonacci;
